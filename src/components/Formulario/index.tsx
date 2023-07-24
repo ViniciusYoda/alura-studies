02a405cf56361d2601ff290dc583d00c8d1665c0
Vinícius Yoda <vyoda4604@gmail.com>
2023-07-24 11:58:47 -0300

    inicio

---
+++ src/components/Formulario/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Botao from '../Botao'
+
+export default function Formulario() {
+    return (
+        <form>
+            <div>
+                <label htmlFor="tarefa">
+                    Adicione um novo estudo
+                </label>
+                <input
+                    type="text"
+                    name="tarefa"
+                    id="tarefa"
+                    placeholder="O que você quer estudar"
+                    required
+                />
+            </div>
+            <div>
+                <label htmlFor="tempo">
+                    Tempo
+                </label>
+                <input
+                    type="time"
+                    step="1"
+                    name="tempo"
+                    id="tempo"
+                    min="00:00:00"
+                    max="01:30:00"
+                    required
+                />
+            </div>
+            <Botao />
+        </form>
+    )
+}
